8e4d2903731ac04c30af86dfba30b865f852cb9c
Abhijitj792001 <abhijithjeejamon792001@gmail.com>
2025-06-12 17:14:55 +0530

    client said change to image section in 'inside the section ' image and contrast in border in freelance stugle section

---
+++ js/script.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'brand-blue': '#0c00ff',
+                'brand-blue-border': '#0C00FF3D',
+                'brand-purple': '#b378cf',
+                'dark-bg': '#01011d',
+                'dark-card': '#040043',
+
+
+                'accent': '#3b82f6',
+                'accent-hover': '#2563eb'
+            },
+            fontFamily: {
+                'aeonik': ['Arial', 'sans-serif']
+            }
+        }
+    }
+}
